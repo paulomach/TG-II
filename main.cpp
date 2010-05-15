ce7c5fe556d509a772b2eb7dca3bf85f3260f88a
Paulo Machado <machado@las.ic.unicamp.br>
2010-05-15 02:25:48 -0300

    First implementation of a main program. When testing, rename it to the right pde extension

---
+++ main.cpp
@@ -0,0 +1,69 @@
+/* 
+ * Main.pde
+ * Version 0.1
+ *
+ * This is file is part of a my final graduation work.
+ * This code is intended to be used with an arduino to 
+ * automatically control gear changes of a commom 
+ * bycicle derailleur.
+ *
+ * Author: Paulo S. Machado
+ * Date: April 2010
+ *
+ * Release under de GNU Public License v3.0 or greater
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include </home/paulo/tmp/arduino-0017/hardware/libraries/LiquidCrystal/LiquidCrystal.h>
+#include "BikeTransmission/BikeTransmission.h"
+
+
+// Objects instatiation
+Servo motor;
+derailleur dera;
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+rear_wheel roda(0.26);
+front_gear pedivela;
+
+// Variables
+long int wheel_speed, cadencia;
+int marcha, speed;
+
+void setup() {
+	motor.attach(9);
+	lcd.begin(16.2);
+	lcd.print("Vel:");
+	lcd.setCursor(7,0);
+	lcd.print("kmph");
+	lcd.setCursor(0,1);
+	lcd.print("Marcha: ");
+}
+
+
+void loop() {
+	wheel_speed = roda.read_wspeed_sensor();
+	cadencia = pedivela.read_cadence_sensor();
+	marcha = dera.get_gear(cadencia, wheel_speed);
+	dera.set_gear(motor, marcha, cadencia);
+	
+	speed = int((1.5708*roda.diameter^2)/wheel_speed);
+	lcd.setCursor(5,0);
+	lcd.print(speed);
+	
+	lcd.setCursor(8,1);
+	lcd.print(marcha);
+}
+
+
